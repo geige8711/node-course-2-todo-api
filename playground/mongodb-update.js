02c1185d1d923bff82945e8b78051e099f0679bf
Yue He <yuehe@Yues-Mac-mini.local>
2018-08-27 22:42:33 +1000

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser: true }, (err, client)=>{
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+    db.collection('Users').findOneAndUpdate({
+        _id : new ObjectID('5b83eeb3b19881a50e70d492')
+    },{
+        $set:{
+            location: 'China'
+        },
+        $inc:{
+            age:2
+        }
+    },{
+        returnOriginal: false
+    }).then((res)=>{
+        console.log(res);
+        // console.log('Todos');
+        // console.log(JSON.stringify(doc, undefined, 2));
+    }).catch((err)=>{
+        console.log('Unable to fetch todos', err);
+    });
+
+});
