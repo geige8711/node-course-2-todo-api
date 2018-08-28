f9772a93af484cdefb85a7fcc52529f81f78e7a0
Yue He <yuehe@Yues-Mac-mini.gateway>
2018-08-28 23:17:44 +1000

    Add POST /todos route and refractor mongoose

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+let app = express();
+const bodyParser = require('body-parser');
+
+const mongoose = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
+
+app.post('/todos', (req, res) => {
+    // console.log(req.body);
+    let todo = new Todo({
+        text:req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+});
+
+
+
+// let newTodo = new Todo({text:'cook dinner'});
+// newTodo.save().then((doc)=>{
+//     console.log(doc);
+// }).catch((e)=>{
+//     console.log('unable to save data', e);
+// });
+
+// let otherTodo = new Todo({
+//     text: '      learn IT Technology     '
+// });
+// otherTodo.save().then((doc)=>{
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }).catch((e)=>{
+//     console.log('unable to save', e);
+// });
